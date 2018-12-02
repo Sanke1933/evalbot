03db1c068bdd3e27b2fb425f6793d6ff7c0aee98
Andrey0189 <39239738+Andrey0189@users.noreply.github.com>
2018-12-02 12:08:45 +0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,9 +2,10 @@
 //Код этого бота свободен для исползования (open-source)
 
 const Discord = require('discord.js'); //Подключение пакета discord.js
+const hastebinGen = require('hastebin-gen');
 const client = new Discord.Client(); //Создание клиента бота
 
-const creators = ['242975403512168449', '406343162651738112']; //Создатели
+const creator = '242975403512168449'; //Мой id
 const prefix = '.' //Префикс
 
 /** @namespace process.env.BOT_TOKEN */ //process.env
@@ -13,45 +14,41 @@ client.on('ready', () => { //Событие запуска клиента
 
     //Вывод информации о боте в консоль
     console.log(`Бот запущен.\nСервера: ${client.guilds.size}.`);
-    
-    client.user.setActivity('Мега токсичное быдло',{ type: 2 });
+
+    client.user.setActivity('Мега токсичное быдло',{ type: 1 });
 
 });
 
 client.on('message', (message) => { //Событие отправки сообщения
 
-    //Игнорирование типов каналов и ботов
-    if(message.channel.type !== `text` || message.author.bot) return;
+    //Игнорирование типов каналов, ботов и других людей
+    if(message.channel.type !== `text` || message.author.bot || message.author.id !== creator) return;
 
     //Константы args и command
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    //Команда eval
+    //Основная команда eval
     if (command === 'js') {
-        
-        if (!creators.includes(message.author.id)) return message.reply('САСАТБ');
-           
+
         const code = args.join(" "); //Константа с ботом
 
         try {
 
             let output = eval(code); //Константа с эмуляцией кода
             
-            if (output.length < 1950) message.author.send(`\`\`\`js\n${output}\n\`\`\``).then(() => {message.react("✅")}); //Отправка результатов симуляции
+            if (output.length < 1950) message.channel.send(`\`\`\`js\n${output}\n\`\`\``).then(() => {message.react("✅")}); //Отправка результатов симуляции
             
             else message.author.send(`${output}`, {split:"\n", code:"js"}); //Отправка результатов симуляции если их длина больше 1950-ти
-            
-            message.delete();
-            
+        
         } 
         
-        catch (error) { message.author.send(`Анхэндлэд промайз риджекшн ворнинг \`\`\`js\n${error}\`\`\``).then(() => message.react("❎")) }; //Отправка ошибки
+        catch (error) { message.channel.send(`Анхэндлэд промайз риджекшн ворнинг \`\`\`js\n${error}\`\`\``).then(() => message.react("❎")) }; //Отправка ошибки
         
     }
 
 
 })
 
-//Реистрация клиента
+//Токен
 client.login(process.env.BOT_TOKEN); 
