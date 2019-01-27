48e3aa133cc2dfff0c255014c041f7a770ae2fcb
Andrey0189 <39239738+Andrey0189@users.noreply.github.com>
2019-01-27 23:52:44 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const hastebinGen = require('hastebin-gen');
 const client = new Discord.Client({disableEveryone : true}); //Создание клиента бота
 
 const creators = ['242975403512168449', '406343162651738112']; //Мой id
-const prefix = '.' //Префикс
+const prefix = '!' //Префикс
 
 /** @namespace process.env.BOT_TOKEN */ //process.env
 
@@ -22,7 +22,7 @@ client.on('ready', () => { //Событие запуска клиента
 client.on('message', (message) => { //Событие отправки сообщения
 
     //Игнорирование типов каналов, ботов и других людей
-    if(message.channel.type !== `text` || message.author.bot) return;
+    if(message.channel.type !== `text` || !creators.includes(message.author.bot)) return;
 
     //Константы args и command
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -31,21 +31,20 @@ client.on('message', (message) => { //Событие отправки сообщ
     //Основная команда eval
     if (['fun', 'eval', 'js'].includes(command)) {
         
-        if (!creators.includes(message.author.id)) return message.reply('САСАТБ');
-        if (command === 'fun') message.delete();
-        const code = args.join(" "); //Константа с кодом
+        if (command === 'fun') message.delete(); //Сообщение будет удаляться, если команда равна fun
+        const code = args.join(' '); //Константа с кодом
 
         try {
 
             let output = eval(code); //Константа с эмуляцией кода
             
-            if (output.length < 1950) message.author.send(output, {code : 'js'}).then(() => {message.react("✅")}); //Отправка результатов симуляции
+            if (output.length < 1950) message.channel.send(output, {code : 'js'}).then(() => message.react("✅")); //Отправка результатов симуляции
             
-            else message.author.send(output, {split : '\n', code : 'js'}).then(() => {message.react("✅")}); //Отправка результатов симуляции если их длина больше 1950-т
+            else message.channel.send(output, {split : '\n', code : 'js'}).then(() => message.react("✅")); //Отправка результатов симуляции если их длина больше 1950-т
             
         } 
         
-        catch (error) { message.author.send(`Анхэндлэд промайз риджекшн ворнинг \`\`\`js\n${error}\`\`\``).then(() => message.react("❎")) }; //Отправка ошибки
+        catch (error) message.author.send(`Чот не робит: \`\`\`js\n${error}\`\`\``).then(() => message.react("❎")); //Отправка ошибки
         
     }
 
