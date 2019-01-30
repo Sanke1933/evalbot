339f89d5be4de1ca80ed507892d95acfb71293eb
Andrey0189 <39239738+Andrey0189@users.noreply.github.com>
2019-01-31 01:06:48 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,45 +55,4 @@ client.on('message', (message) => { //Событие отправки сообщ
 })
 
 //Токен
-client.login(process.env.BOT_TOKEN);ient.guilds.size}.`);
-    //Бот будет показывать сколкьо памяти анимает его процесс
-    setInterval(() => client.user.setActivity(`${Math.round(process.memoryUsage().rss / 1024 / 1024 )}/1024 МБ`, { type: 'PLAYING' }), 16000);
-
-});
-
-client.on('message', (message) => { //Событие отправки сообщения
-
-    //Игнорирование типов каналов, ботов и других людей
-    if(message.channel.type !== 'text' || message.author.bot || !message.content.startsWith(prefix)) return;
-
-    //Константы args и command
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    //Основная команда eval
-    if (['fun', 'eval', 'js'].includes(command)) {
-        
-        if (!creators.includes(message.author.id)) return message.reply('САСАТБ');
-        
-        if (command === 'fun') message.delete(); //Сообщение будет удаляться, если команда равна fun
-        
-        const code = args.join(' '); //Константа с кодом
-
-        try {
-
-            let output = eval(code); //Константа с эмуляцией кода
-            
-            if (output.length < 1950) message.channel.send(output, {code : 'js'}).then(() => message.react("✅")); //Отправка результатов симуляции
-            
-            else message.channel.send(output, {split : '\n', code : 'js'}).then(() => message.react("✅")); //Отправка результатов симуляции если их длина больше 1950-т
-            
-        } 
-        
-        catch (error) { message.author.send(`Чот не робит: \`\`\`js\n${error}\`\`\``).then(() => message.react("❎")) }; //Отправка ошибки
-        
-    }
-
-})
-
-//Токен
 client.login(process.env.BOT_TOKEN); 
