40f8034639e3d468a2c4ba7a71a20d993acb68f9
Andrey0189 <39239738+Andrey0189@users.noreply.github.com>
2019-01-31 01:04:25 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,64 @@ const prefix = '!' //Префикс
 client.on('ready', () => { //Событие запуска клиента
 
     //Вывод информации о боте в консоль
+    console.log(`Бот запущен.\nСервера: ${cl//ВНИМАНИЕ!
+//Код этого бота свободен для исползования (open-source)
+
+const Discord = require('discord.js'); //Подключение пакета discord.js
+const hastebinGen = require('hastebin-gen');
+const client = new Discord.Client({disableEveryone : true}); //Создание клиента бота
+
+const creators = ['242975403512168449', '406343162651738112', '401739659945967626', '421030089732653057']; //Вайтлист
+const prefix = '!' //Префикс
+
+/** @namespace process.env.BOT_TOKEN */ //process.env
+
+client.on('ready', () => { //Событие запуска клиента
+
+    //Вывод информации о боте в консоль
     console.log(`Бот запущен.\nСервера: ${client.guilds.size}.`);
+    //Бот будет показывать сколкьо памяти занимает его процесс
+    setInterval(() => client.user.setActivity(`${Math.round(process.memoryUsage().rss / 1024 / 1024 )}/1024 МБ`, { type: 'PLAYING' }), 16000);
+
+});
+
+client.on('message', (message) => { //Событие отправки сообщения
+
+    //Игнорирование типов каналов и ботов
+    if(message.channel.type !== 'text' || message.author.bot || !message.content.startsWith(prefix)) return;
+
+    //Константы args и command
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    //Основная команда eval
+    if (['beval', 'eval', 'js'].includes(command)) {
+
+        try {
+
+            if (!creators.includes(message.author.id) && ['eval', 'js'].includes(command)) return message.reply('САСАТБ');
+        
+            const code = args.join(' '); //Константа с кодом
+
+            let output //Переменная с эмуляцией кода
+
+            if (['eval', 'js'].includes(command)) output = eval(code);
+            else output = client._eval(code);
+            
+            if (output.length < 1950) message.channel.send(output, {code : 'js'}).then(() => message.react("✅")); //Отправка результатов симуляции
+            
+            else message.channel.send(output, {split : '\n', code : 'js'}).then(() => message.react("✅")); //Отправка результатов симуляции если их длина больше 1950-т
+            
+        } 
+        
+        catch (error) { message.author.send(`Чот не робит: \`\`\`js\n${error}\`\`\``).then(() => message.react("❎")) }; //Отправка ошибки
+        
+    }
+
+})
+
+//Токен
+client.login(process.env.BOT_TOKEN);ient.guilds.size}.`);
     //Бот будет показывать сколкьо памяти анимает его процесс
     setInterval(() => client.user.setActivity(`${Math.round(process.memoryUsage().rss / 1024 / 1024 )}/1024 МБ`, { type: 'PLAYING' }), 16000);
 
@@ -50,7 +107,6 @@ client.on('message', (message) => { //Событие отправки сообщ
         catch (error) { message.author.send(`Чот не робит: \`\`\`js\n${error}\`\`\``).then(() => message.react("❎")) }; //Отправка ошибки
         
     }
-
 
 })
 
